97ea3fcdbe47d24e77371d1a377f0dd90e1bb3dc
Daniel-jsx <167225409+VestingPartners@users.noreply.github.com>
2024-07-29 13:14:53 -0400

    3

---
+++ .next/static/iuAZWuGsLlBPfpQZDlyom/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-a9cb264b8606e445.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
